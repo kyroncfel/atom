27c373d4e100ae010160165ce27e3a64c6d3fbd2
David Wilson <daviwil@github.com>
2018-08-03 07:28:31 -0700

    Remove `--global-style` flag from apm installation
    This should improve deduping of apm's dependencies without actually 
    having to run `npm dedupe`.

--- script/lib/install-apm.js
+++ script/lib/install-apm.js
@@ -8,16 +8,7 @@ module.exports = function () {
   console.log('Installing apm')
   childProcess.execFileSync(
     CONFIG.getNpmBinPath(),
-    ['--global-style', '--loglevel=error', 'install'],
+    ['--loglevel=error', 'install'],
     {env: process.env, cwd: CONFIG.apmRootPath}
   )
-
-  if (process.platform === 'win32') {
-    console.log('Deduping apm dependencies (Windows only)')
-    childProcess.execFileSync(
-      CONFIG.getNpmBinPath(),
-      ['--loglevel=error', 'dedupe'],
-      {env: process.env, cwd: CONFIG.apmRootPath}
-    )
-  }
 }
